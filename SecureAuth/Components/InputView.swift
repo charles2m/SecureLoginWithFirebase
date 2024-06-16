a902ce8dc7d3051d5ac95e3f43eafa81fd62a1e9
charles2m <ch.mmartiniere@hotmail.com>
2024-06-16 11:52:00 +0200

    [Feature]LoginViewUI

---
+++ SecureAuth/Components/InputView.swift
@@ -0,0 +1,18 @@
+//
+//  InputView.swift
+//  SecureAuth
+//
+//  Created by Charles Martin Martinière on 16/06/2024.
+//
+
+import SwiftUI
+
+struct InputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InputView()
+}
