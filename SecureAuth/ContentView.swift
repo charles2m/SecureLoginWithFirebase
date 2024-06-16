7afbe6e4a19272e0f1ba7aee68b2e293af2cf4f3
charles2m <ch.mmartiniere@hotmail.com>
2024-06-16 09:34:47 +0200

    Initial Commit

---
+++ SecureAuth/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SecureAuth
+//
+//  Created by Charles Martin Martinière on 16/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
